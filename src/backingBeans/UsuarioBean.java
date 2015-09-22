f5f8fbde3da55472c6cb0bffe32015a69e5c4868
Xavier <Xavier@pcxavier>
2015-09-22 19:13:31 -0300

    ABM Usuarios pero "Lindo"

--- src/backingBeans/UsuarioBean.java
+++ src/backingBeans/UsuarioBean.java
@@ -1,55 +1,42 @@
 package backingBeans;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-
-//import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-
 import dao.FactoryDAO;
 import model.Perfil;
 import model.Usuario;
+
 
 public class UsuarioBean implements Serializable{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	//att
+
 	private long id;	
-	private Perfil perfil;
 	private Perfil[] perfilesDisponibles;
 	private Usuario usuario;
-	private String oper;
-	private boolean esAlta;	
+	private Operacion oper;
 	
-
-	public boolean getEsAlta() {
-		return esAlta;
-	}
-
-	public void setEsAlta(boolean esAlta) {
-		this.esAlta = esAlta;
-	}
-
-	public String getOper() {
-		return oper;
-	}
-
-	public void setOper(String oper) {
-		this.oper = oper;
-	}
-
-	//const
-	public UsuarioBean(){		
+	public UsuarioBean() {		
 		perfilesDisponibles = Perfil.values();
 
 	}
 	
-	//setters and getters
+	public Perfil[] getPerfilesDisponibles(){		
+		return this.perfilesDisponibles;
+	}	
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	
+	public boolean getEsAlta() {
+		return (oper == Operacion.NUEVO_USUARIO) || (oper == Operacion.REGISTRARSE);
+	}
+
 	public long getId() {
 		return id;
 	}
@@ -57,95 +44,136 @@ public class UsuarioBean implements Serializable{
 	public void setId(long id) {
 		this.id = id;
 	}
+	
+
+	
+	public String editarUsuario() {
+		oper = Operacion.EDITAR_USUARIO;
 		
-	public Perfil getPerfil() {
-		return perfil;
-	}
-	
-	public Perfil[] getPerfilesDisponibles(){		
-		return this.perfilesDisponibles;
-	}
-
-	public void setPerfil(Perfil perfil) {
-		this.perfil = perfil;
-	}		
-	
-	
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(Usuario usuario2) {
-		this.usuario = usuario2;
-	}
-	
-	public String editar(){
+		// Recupero el idUsuario pasado x parametro
 		Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-        long selectedEntityId = Long.parseLong(params.get("id"));        
-        this.setOper("M"); 
-        this.setEsAlta(false);
+        long idUsuario = Long.parseLong(params.get("id"));        
+                
    	    //cargar usuario por id        	
-   	    this.setUsuario(FactoryDAO.getUsuarioDAO().buscaPorID(selectedEntityId));
-   	    id = selectedEntityId;
-   	    this.perfil = this.getUsuario().getPerfil();
+   	    usuario = FactoryDAO.getUsuarioDAO().buscaPorID(idUsuario);
+
+   	    
    	    return "/usuario.xhtml?faces-redirect=true";
 	}
 	
-	public String eliminar(){
-		this.setEsAlta(false);
-   	    //cargar usuario por id  
+	public String eliminarUsuario(){
+		oper = Operacion.ELIMINAR_USUARIO;
+		
+		// Recupero el idUsuario pasado x parametro 
 		Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-        long selectedEntityId = Long.parseLong(params.get("id"));        
-   	    this.setUsuario(FactoryDAO.getUsuarioDAO().buscaPorID(selectedEntityId));
-   	    FactoryDAO.getUsuarioDAO().borrar(this.getUsuario());
+        long idUsuario = Long.parseLong(params.get("id"));   
+        
+        // cargar usuario por id 
+   	    usuario = FactoryDAO.getUsuarioDAO().buscaPorID(idUsuario);
+   	    
+   	    // Borro el usuario
+   	    FactoryDAO.getUsuarioDAO().borrar(usuario);
+   	    
+   	    // Retorno al listado de usuarios (solo el admin puede borrar usuarios desde el listado de usuarios)
 		return "/usuarios.xhtml?faces-redirect=true"; 
 	}
 	
-	private boolean guardar(){
-		//crea el dao y persiste al nuevo usuario		
-		if(this.getUsuario().getId() == 0){ //alta			
-		  FactoryDAO.getUsuarioDAO().persistir(this.getUsuario());
-		}
-		else{ //modificación
-			/*if(perfil.equals("administrador")){
-				usuario.setPerfil(FactoryDAO.getPerfilDAO().buscaPorID(1));
-			} 
-			else{
-				usuario.setPerfil(FactoryDAO.getPerfilDAO().buscaPorID(2));
-			}*/
-			usuario.setPerfil(perfil);
-			FactoryDAO.getUsuarioDAO().update(this.getUsuario());
-			} 		
-		return true;
-	}
-	
-	public String nuevo(){		
-        this.setOper("A"); 
-        this.setEsAlta(true);
-    	this.setUsuario(new Usuario());
-    	this.getUsuario().setId(0);
-    	this.getUsuario().setPerfil(Perfil.USUARIO);
-    	this.getUsuario().setActivo(true);
+
+	public String nuevoUsuario(){		
+		oper = Operacion.NUEVO_USUARIO; 
+		
+    	usuario = new Usuario();    	
+    	usuario.setPerfil(Perfil.USUARIO);
+    	usuario.setActivo(true);
+    	    	
 		return "/usuario.xhtml?faces-redirect=true";
 	}
 	
-	public String registrar(){
+	public String modificarDatos(){		
+		oper = Operacion.MODIFICAR_DATOS; 
+		
+		// Recupero el idUsuario pasado x parametro 
+		Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        long idUsuario = Long.parseLong(params.get("id"));   
+        
+        // cargar usuario por id 
+   	    usuario = FactoryDAO.getUsuarioDAO().buscaPorID(idUsuario);
+		
+		return "/usuario.xhtml?faces-redirect=true";
+	}
+	
+	public String registrarse(){		
+		oper = Operacion.REGISTRARSE; 
+		
+		usuario = new Usuario();
+		usuario.setPerfil(Perfil.USUARIO);
+		usuario.setActivo(true);
+		
+		return "/usuario.xhtml?faces-redirect=true";
+	}
+	
+	public String guardar(){
+		
 		try{		  
-		  this.guardar();
-		  if(this.getOper().equals("A")){
-			  return "success";
-			  }
-		  else{			 			 
-			 return "/bienvenido.xhtml?faces-redirect=true"; 
-		  }
+			this.persistir();
+			
+			switch (oper) {
+				case NUEVO_USUARIO:
+					return "/usuarios.xhtml?faces-redirect=true"; 
+					
+				case EDITAR_USUARIO:
+					return "/usuarios.xhtml?faces-redirect=true"; 	
+					
+				case REGISTRARSE:
+					return "/login.xhtml?faces-redirect=true"; 
+					
+				default:
+					return "/bienvenido.xhtml?faces-redirect=true"; 
+			}
+		  
+
 		}
 		catch(Exception e){
-			//algo dio error
+			// TODO revisar que hacer cuando da error
 			FacesContext context = FacesContext.getCurrentInstance();
 			FacesMessage message = new FacesMessage("Invalid Username and/or Password.");
 			context.addMessage("newUserForm", message);
 			return "failure";
 		}				
-	}		
+	}	
+	
+	public String getCancelarURL(){
+		
+		switch (oper) {
+			case NUEVO_USUARIO:
+				return "./usuarios.xhtml?faces-redirect=true"; 
+				
+			case EDITAR_USUARIO:
+				return "./usuarios.xhtml?faces-redirect=true"; 	
+				
+			case REGISTRARSE:
+				return "./login.xhtml?faces-redirect=true"; 	
+				
+			default:
+				return "./bienvenido.xhtml?faces-redirect=true"; 
+		}
+			
+	}	
 
+	
+	private boolean persistir(){
+		//crea el dao y persiste al nuevo usuario		
+		if(this.getEsAlta()) { 
+			//alta			
+			FactoryDAO.getUsuarioDAO().persistir(this.getUsuario());
+		}
+		else { 
+			//modificacion			
+			FactoryDAO.getUsuarioDAO().update(this.getUsuario());
+		} 		
+		return true;
+	}
+
+	private enum Operacion {REGISTRARSE, MODIFICAR_DATOS, NUEVO_USUARIO, EDITAR_USUARIO, ELIMINAR_USUARIO}
+	
 }
